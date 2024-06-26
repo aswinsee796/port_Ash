91f50713db62fd3b6c2ccd9b151873ec216bd8b5
ASWIN S <aswinsee796@gmail.com>
2024-06-26 20:33:08 +0530

    final

---
+++ port/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure the output directory is 'dist' which is the default for Vite
+  },
+  
+})
